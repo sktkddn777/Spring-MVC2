44155dfef162b43dde4c2b4d82575ddbfe70b59e
한상우 <sangwoha@Sangwoha-MacBook-Air.local>
2022-09-20 21:38:51 +0900

    Add: 타임 리프 변수 사용 방법
    다양한 방법으로 넘어온 model에 담긴 값을 알 수 있다
    + 지역 변수 선언도 가능하다

--- thymeleaf-basic/src/main/java/hello/thymeleafbasic/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleafbasic/basic/BasicController.java
@@ -1,9 +1,16 @@
 package hello.thymeleafbasic.basic;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/basic")
@@ -19,5 +26,35 @@ public class BasicController {
     public String textUnescaped(Model model) {
         model.addAttribute("data", "Hello <b>Spring</b>");
         return "basic/text-unescaped";
+    }
+
+    @GetMapping("/variable")
+    public String variable(Model model) {
+        // case 1
+        User userA = new User("userA", 10);
+        User userB = new User("userB", 20);
+
+        // case 2
+        List<User> list = new ArrayList<>();
+        list.add(userA);
+        list.add(userB);
+
+        // case 3
+        Map<String, User> map = new HashMap<>();
+        map.put("userA", userA);
+        map.put("userB", userB);
+
+        model.addAttribute("user", userA);
+        model.addAttribute("users", list);
+        model.addAttribute("userMap", map);
+
+        return "basic/variable";
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class User {
+        private String username;
+        private int age;
     }
 }
