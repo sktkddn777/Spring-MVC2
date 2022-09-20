fa07bd587bfd25b7c74b2fed810b9b7278f2d5a1
한상우 <sangwoha@Sangwoha-MacBook-Air.local>
2022-09-20 22:25:55 +0900

    Add: 타임리프 url 경로
    쿼리 파라미터, 경로 변수로 값을 넣어줄 수 있다

--- thymeleaf-basic/src/main/java/hello/thymeleafbasic/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleafbasic/basic/BasicController.java
@@ -73,6 +73,14 @@ public class BasicController {
         return "basic/date";
     }
 
+    @GetMapping("/link")
+    public String link(Model model) {
+        model.addAttribute("param1", "data1");
+        model.addAttribute("param2", "data2");
+
+        return "basic/link";
+    }
+
     @Data
     @AllArgsConstructor
     static class User {
