e561aad5ac1ff54e5160ce4cc6d018cd995eaccb
한상우 <sangwoha@Sangwoha-MacBook-Air.local>
2022-09-24 14:52:32 +0900

    Update: 직접 만든 세션 적용

--- login-start/src/main/java/hello/login/web/HomeController.java
+++ login-start/src/main/java/hello/login/web/HomeController.java
@@ -2,6 +2,7 @@ package hello.login.web;
 
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
+import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -9,25 +10,26 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Slf4j
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
 
     private final MemberRepository memberRepository;
+    private final SessionManager sessionManager;
 
     @GetMapping("/")
-    public String home(@CookieValue(name = "memberId", required = false) Long memberId, Model model) {
-        if (memberId == null) {
-            return "home";
-        }
+    public String home(HttpServletRequest request, Long memberId, Model model) {
 
-        Member loginMember = memberRepository.findById(memberId);
-        if (loginMember == null) {
-            return "home";
-        }
+        // session 에 저장된 정보 조회
+        Member member = (Member)sessionManager.getSession(request);
 
-        model.addAttribute("member", loginMember);
+        if (member == null)
+            return "home";
+
+        model.addAttribute("member", member);
         return "loginHome";
     }
 }
