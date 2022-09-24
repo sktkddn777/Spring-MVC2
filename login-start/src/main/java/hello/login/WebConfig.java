701d525ae2967a2f3d03b6cb9d71b5be690bb7d5
한상우 <sangwoha@Sangwoha-MacBook-Air.local>
2022-09-24 22:26:16 +0900

    Add: Spring 인터셉터
    인터셉터는 컨트롤러 호출 직전에 호출된다
    체인 가능, 서블릿 필터보다 더 정교하고 다양한 기능 제공
    HandlerInterceptor 인터페이스 구현하면 된다
    preHandle: 컨트롤러 호출 전 (핸들러 정보가 넘어옴)
    postHandle: 컨트롤러 호출 후 (에러나면 호출 x)
    afterCompletion: 뷰가 렌더링 된 이후 (항상 호출)

--- login-start/src/main/java/hello/login/WebConfig.java
+++ login-start/src/main/java/hello/login/WebConfig.java
@@ -2,16 +2,27 @@ package hello.login;
 
 import hello.login.web.filter.LogFilter;
 import hello.login.web.filter.LoginCheckFilter;
+import hello.login.web.interceptor.LogInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.Filter;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/*.ico", "/error");
+    }
+
+//    @Bean
     public FilterRegistrationBean logFilter() {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LogFilter());
@@ -22,7 +33,7 @@ public class WebConfig {
     }
 
     // filter 호출을 많이 한다고 성능에 영향을 미치진 않음. 그냥 메소드 하나 호출 같은 느낌
-    @Bean
+//    @Bean
     public FilterRegistrationBean loginCheckFilter() {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LoginCheckFilter());
