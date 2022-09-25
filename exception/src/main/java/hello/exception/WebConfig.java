f76713d5f871f2765523911020962a0231d5d98b
한상우 <sangwoha@Sangwoha-MacBook-Air.local>
2022-09-25 16:54:41 +0900

    Update: 서블릿 예외처리 - 필터
    인증 같은 체크일 경우 이미 필터에서 체크 완료했기에
    오류 페이지 호출시 다시 호출되는건 비효율적
    필터는 기본이 DispatcherType이 Request일 때 호출된다

---
+++ exception/src/main/java/hello/exception/WebConfig.java
@@ -0,0 +1,27 @@
+package hello.exception;
+
+import hello.exception.filter.LogFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.Filter;
+
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public FilterRegistrationBean logFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LogFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ERROR);
+        // 이 필터는 request, error 인 경우에 호출이 된다
+        // 필터는 기본이 request 여서 아무 설정 안해주면 request 일 때만 호출된다
+        return filterRegistrationBean;
+    }
+}
