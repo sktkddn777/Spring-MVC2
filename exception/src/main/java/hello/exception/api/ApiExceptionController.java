e7d52846f82bdd9b1d9cd17e79dd2ba2a63bcf2f
한상우 <sangwoha@Sangwoha-MacBook-Air.local>
2022-09-27 15:13:29 +0900

    Add: ResponseStatusExceptionResolver
    HTTP 상태코드를 지정할 수 있다. 내부 코드를 보면 결국 sendError를 호출하기에
    WAS에서 다시 오류 페이지(/error)를 요청한다

--- exception/src/main/java/hello/exception/api/ApiExceptionController.java
+++ exception/src/main/java/hello/exception/api/ApiExceptionController.java
@@ -1,12 +1,15 @@
 package hello.exception.api;
 
+import hello.exception.exception.BadRequestException;
 import hello.exception.exception.UserException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @Slf4j
 @RestController
@@ -23,6 +26,18 @@ public class ApiExceptionController {
         if (id.equals("user-ex"))
             throw new UserException("사용자 오류");
         return new MemberDto(id, "name"+id);
+    }
+
+    @GetMapping("/api/response-status-ex1")
+    public String responseStatusEx1() {
+        throw new BadRequestException();
+    }
+
+    @GetMapping("/api/response-status-ex2")
+    public String responseStatusEx2() {
+        // 특수한 exception 이 있음, 상태코드와 메시지 보내기
+        // ResponseStatus 는 동적으로 변경하기 어렵기 때문에 아래와 같은 특수한 예외를 보내야 함
+        throw new ResponseStatusException(HttpStatus.FORBIDDEN, "권한이 없습니다");
     }
 
     @Data
